03bffd4bcaf02c486986eec2c092c1fea1cd4f4c
Sachiko Yamasaki <sachiko6248@gmail.com>
2019-01-08 20:06:25 -0800

    Finalize API and display

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,29 +2,40 @@
 const ingredients = [];
 const clarifi_model = 'bd367be194cf45149e75f01d59f77ba7'
 const image_URL = 'https://food.fnr.sndimg.com/content/dam/images/food/unsized/2016/2/16/0/FND_chefs-picks-comfort-madison-poutine_s4x3.jpg'
+var nutritionix_input = [];
 
-// instantiate a new Clarifai app passing in the API key.
+// Instantiate a new Clarifai app passing in the API key.
 const app = new Clarifai.App({
   apiKey: '080661d621c8443089fdf99c3df14ae1'
 });
 
+// Format Clarifi results before sending to Nutritionix
+function format4NutritionixAPI (clarifi_response){
+
+  for (let i = 0; i < 10; i++) {
+    ingredients[i] = {
+      name: clarifi_response.outputs[0].data.concepts[i].name, 
+      value: clarifi_response.outputs[0].data.concepts[i].value
+    };
+  }
+
+  ingredients.forEach(element => {
+    console.log(element.name);
+    nutritionix_input = nutritionix_input.concat(' ' + element.name + ' and ');
+  });
+
+    return nutritionix_input
+}
+
 //use Clarifi.GENERAL_MODEL to access genereric prediction model
 app.models.predict(clarifi_model, image_URL).then(
     
-    function(response) {
-
-      for (let i = 0; i < 10; i++) {
-        ingredients[i] = {
-          name: response.outputs[0].data.concepts[i].name, 
-          value: response.outputs[0].data.concepts[i].value
-        };
-      }
-
-      console.log('This is the ingredients array: ' + JSON.stringify(ingredients));
-      console.log(ingredients);
-      console.log(ingredients[3].name); 
-      console.log(ingredients[3].value); 
-
+    function(clarifi_response) {
+      
+      var nutritionix_input = format4NutritionixAPI(clarifi_response);
+      console.log('This is the nutritionix_input string :'+ nutritionix_input);
+      callEdamam(nutritionix_input);
+    
     },
     //catch errors
     function(err) {
@@ -34,13 +45,14 @@ app.models.predict(clarifi_model, image_URL).then(
 
 );
 
+
 //by Federico Villalpando 12/20
 const nutritionix_app_ID = '91b04efd';
 const nutritionix_app_KEY = '86859fed8878066519436c598b5c1c23';
 const queryURL = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
 
-function callEdamam(){
-    
+function callEdamam(nutritionix_input){
+    //var test = nutritionix_input.toString();
     var settings = 
         {
             "async": true,
@@ -55,18 +67,38 @@ function callEdamam(){
                 "Postman-Token": "a40402d4-a942-4f20-801d-df21a2a44301"
             },
             "processData": false,
-            "data": "{\n\t\"query\":\"for breakfast i ate 2 eggs, bacon, and french toast\"}"
+            "data": "{\"query\":\" "+ nutritionix_input + " pesto,pasta,penne,meat,garlic\"}"
         }
 
         $.ajax(settings).then(
-            function (response) {
-            console.log(response);
+            function (nutritionix_response) {
+
+              displayIngredients(nutritionix_response);
 
         });      
+        
+};
 
-        // displayIngredients(ingredients);   Sachiko 
+function displayIngredients(nutritionix_response){
+  console.log('This is the display Function [Sachiko to display the contents of the nutritionix_response object]:')
+  var food = nutritionix_response;
+  console.log(food.foods[1].food_name);
+  for (i = 0; i < food.foods.length; i++) {
+    console.log(food.foods[i]);
+    console.log(food.foods[i].nf_calories);
+    var newRow = $("<tr>").append(
+      $("<td>").text(food.foods[i].food_name),
+      $("<td>").text(food.foods[i].serving_qty),
+      $("<td>").text(food.foods[i].serving_unit),
+      $("<td>").text(food.foods[i].nf_calories),
+      $("<td>").text(food.foods[i].nf_protein),
+      $("<td>").text(food.foods[i].nf_total_carbohydrate),
+      $("<td>").text(food.foods[i].nf_total_fat),
+    );
+    $("#ingredients").append(newRow);
+    }
 
-        // ingredients = {
+  // use nutritionix_response objec to create a table that displays:
         //     link_to_thumbnail: '',       // thubmnail 
         //     name: '',                    //name of item
         //     calories: '',                //total calories:  id: 208 in kcal
@@ -76,8 +108,12 @@ function callEdamam(){
         //     vitamin_D: ''                // total vit D : id: 328 in micro grams
         // }
 
-};
 
-callEdamam();
+}
+
+
+
+
+
 
 
